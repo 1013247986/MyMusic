81962367113b954918b12c0d91f50ad173a079a2
e <1013247986@qq.com>
2020-03-07 15:47:37 +0800

    解决用户授权

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -5,7 +5,7 @@ Page({
     back: app.globalData.back, //背景色
     colors: app.globalData.colors, //字体颜色
     boxcolor: app.globalData.boxcolor, // 盒子背景色
-    username: "",
+    username: undefined,
     userimg: "",
   },
 
@@ -13,10 +13,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      userimg: app.globalData.usertopimg,
-      username: app.globalData.username
-    })
+    console.log(app.globalData.usertopimg)
+    if (app.globalData.usertopimg !== null && app.globalData.username !== null){
+      this.setData({
+        userimg: app.globalData.usertopimg,
+        username: app.globalData.username
+      })
+    }
+
   },
 
   /**
